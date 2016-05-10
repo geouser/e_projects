832fc19aafbfe6997b0572840b37e8f33213b503
OlehRusyi <olehrusy@gmail,com>
2016-05-10 15:33:30 +0300

    чі

--- js/main.js
+++ js/main.js
@@ -68,7 +68,13 @@ if( $('.fullpage').length > 0) {
           onLeave: function(index, nextIndex, direction){},
           afterLoad: function(anchorLink, index){},
           afterRender: function(){},
-          afterResize: function(){},
+          afterResize: function(){
+            if ($(window).width() <= 1200) {
+              $('link[href="https://cdn.jsdelivr.net/jquery.fullpage/2.5.9/jquery.fullPage.min.css"]').attr('href', '');
+            } if ($(window).width() >= 1201) {
+              $('body').append('<link rel="stylesheet" href="https://cdn.jsdelivr.net/jquery.fullpage/2.5.9/jquery.fullPage.min.css">');
+            }
+          },
           afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
           onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
       });
@@ -78,96 +84,10 @@ if( $('.fullpage').length > 0) {
     }
   }
 
-$(window).resize(function() {
-if ($(window).width() >= 1201) {
-    $('body').append('<link rel="stylesheet" href="https://cdn.jsdelivr.net/jquery.fullpage/2.5.9/jquery.fullPage.min.css">');
-    if( $('.fullpage').length > 0) {
-      $('.fullpage').fullpage({
-          //Navigation
-          //menu: '#menu',
-          lockAnchors: false,
-          //anchors:['firstPage', 'secondPage'],
-          navigation: false,
-          navigationPosition: 'right',
-          //navigationTooltips: ['firstSlide', 'secondSlide'],
-          showActiveTooltip: false,
-          slidesNavigation: true,
-          slidesNavPosition: 'bottom',
-
-          //Scrolling
-          css3: true,
-          scrollingSpeed: 1500,
-          autoScrolling: true,
-          fitToSection: true,
-          fitToSectionDelay: 0,
-          scrollBar: false,
-          easing: 'easeInOutCubic',
-          easingcss3: 'ease',
-          loopBottom: false,
-          loopTop: false,
-          loopHorizontal: true,
-          continuousVertical: false,
-          //normalScrollElements: '#element1, .element2',
-          scrollOverflow: false,
-          touchSensitivity: 15,
-          normalScrollElementTouchThreshold: 5,
-
-          //Accessibility
-          keyboardScrolling: true,
-          animateAnchor: true,
-          recordHistory: true,
-
-          //Design
-          controlArrows: true,
-          verticalCentered: true,
-          resize : false,
-          //sectionsColor : ['#ccc', '#fff'],
-          paddingTop: '0',
-          paddingBottom: '0',
-          //fixedElements: 'header, .footer',
-          responsiveWidth: 0,
-          responsiveHeight: 0,
-
-          //Custom selectors
-          sectionSelector: 'section',
-          slideSelector: '.fp-slide',
-
-          //events
-          onLeave: function(index, nextIndex, direction){},
-          afterLoad: function(anchorLink, index){},
-          afterRender: function(){},
-          afterResize: function(){},
-          afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-          onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
-      });
-      $(document).on('click', '.scroll_down', function(){
-        $.fn.fullpage.moveSectionDown();
-      });
-    }
-} else {
-    $.fn.fullpage.destroy('all');
-    $('link[href="https://cdn.jsdelivr.net/jquery.fullpage/2.5.9/jquery.fullPage.min.css"]').attr('href', '');
-}
-});
-
-
 $('.scrollBar').perfectScrollbar({
   suppressScrollX: true
 });
-/*---------------------------
-                              ADD CLASS ON SCROLL
----------------------------*/
-$(function() { 
-  var $document = $(document),
-      $element = $('.menu-button'),
-      $element2 = $('header'),
-      className = 'hasScrolled';
 
-  $document.scroll(function() {
-    $element.toggleClass(className, $document.scrollTop() >= 1);
-    $element2.toggleClass(className, $document.scrollTop() >= 1);
-  });
-});
 
 
 /*---------------------------
@@ -183,6 +103,11 @@ $('.menu-button').on('click', function(event) {
       $('body, html').css('overflow', 'visible');
     }
 });
+
+
+/*---------------------------
+                              Sliders
+---------------------------*/
 
 $('.trustUs__comp').slick({
   arrows: false,
